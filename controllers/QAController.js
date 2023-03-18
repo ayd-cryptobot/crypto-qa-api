291aaf5b7c35b123681117bea4896165e4ec9670
JohanLeonMilenium <123989046+JohanLeonMilenium@users.noreply.github.com>
2023-03-18 11:35:25 -0500

    agregando endpoint de openai

--- controllers/QAController.js
+++ controllers/QAController.js
@@ -1,57 +1,103 @@
-const conexion = require('../database/db')
+const conexion = require("../database/db");
+const apiKey = process.env.OPENAI_API_KEY;
+const axios = require('axios');
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 exports.ConsultQA = async (req, res) => {
-    try {
-        conexion.query('SELECT * FROM qa', async (err, results) =>{
-            res.send(results)
-        })
-    } catch (error) {
-        console.log(error)
-    }
-        
-}
+  try {
+    conexion.query("SELECT * FROM qa", async (err, results) => {
+      res.send(results);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.ConsultQAById = async (req, res) => {
+  try {
+    conexion.query(
+      "SELECT * FROM qa where id=?",
+      [req.params.id],
+      async (err, results) => {
+        res.send(results);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 exports.CreateQA = async (req, res) => {
+  try {
+    const question = req.body.question;
+    const answer = req.body.answer;
+    const data = {
+      question,
+      answer,
+    };
+    conexion.query("INSERT INTO qa SET ?", data, (err, conn) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(req.body);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.generateText = async (req, res) => {
     try {
-        const question = req.body.question
-        const answer = req.body.answer
-        const data = {
-            question,
-            answer
+        const completion = await openai.createCompletion({
+          model: "text-davinci-003",
+          prompt: req.body.text,
+          max_tokens: 300,
+        });
+        res.status(200).json({ result: completion.data.choices[0].text });
+      } catch(error) {
+        // Consider adjusting the error handling logic for your use case
+        if (error.response) {
+          console.error(error.response.status, error.response.data);
+          res.status(error.response.status).json(error.response.data);
+        } else {
+          console.error(`Error with OpenAI API request: ${error.message}`);
+          res.status(500).json({
+            error: {
+              message: 'An error occurred during your request.',
+            }
+          });
         }
-        conexion.query('INSERT INTO qa SET ?', data, (err, conn) =>{
-            if(err) {console.log(err)}
-            res.send('Q&A create successfully')
-        })
-    } catch (error) {
-        console.log(error)
-    }
-        
-}
+      }
+};
 
 exports.EditQA = async (req, res) => {
-    try {
-        console.log(req.body)
-        conexion.query('UPDATE qa set ? WHERE id = ?', [req.body, req.params.id], (err, conn)=>{
-            if(err) return res.send(err)
-
-            res.send('Q&A edit successfully')
-        })
-    } catch (error) {
-        console.log(error)
-    }
-        
-}
+  try {
+    conexion.query(
+      "UPDATE qa set ? WHERE id = ?",
+      [req.body, req.params.id],
+      (err, conn) => {
+        res.send(req.body);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 exports.DeleteQA = async (req, res) => {
-    try {
-        conexion.query('DELETE FROM qa WHERE id = ?', [req.params.id], (err, conn)=>{
-            if(err) return res.send(err)
-
-            res.send('Q&A delete successfully')
-        })
-    } catch (error) {
-        console.log(error)
-    }
-        
-}
+  try {
+    conexion.query(
+      "DELETE FROM qa WHERE id = ?",
+      [req.params.id],
+      (err, conn) => {
+        res.send({});
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,9 @@ const router = express.Router()
 const QAController = require('../controllers/QAController')
 
 router.get('/qa/consult', QAController.ConsultQA)
+router.get('/qa/consult/:id', QAController.ConsultQAById)
 router.post('/qa/create', QAController.CreateQA)
+router.post('/qa/ia/text-generator', QAController.generateText)
 router.put('/qa/edit/:id', QAController.EditQA)
 router.delete('/qa/delete/:id', QAController.DeleteQA)
 
